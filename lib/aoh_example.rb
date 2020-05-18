76bd2d61ce75ca8bc64cea8bd28a03303216c87f
Troy Domio <troyakins99@yahoo.com>
2020-05-18 21:05:10 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -34,6 +34,28 @@ def assembled_aoh
 end
 
 def literal_aoh
+  ppl = [
+    {
+      :name =>"Don Gately" ,
+      :occupation => "Live-in Staff"
+    },
+  {
+      :name =>"Joelle van Dyne" ,
+      :occupation => "Radio Personality"
+    },
+   {
+      :name =>"Pat Monteseian" ,
+      :occupation => "Staff"
+    },
+   {
+      :name =>"Kate Gompert" ,
+      :occupation => "None"
+    },
+   {
+      :name =>"Bruce Green" ,
+      :occupation => "Fan of Mildred"
+    }
+    ]
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
 end
